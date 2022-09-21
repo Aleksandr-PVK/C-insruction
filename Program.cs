f6c3e239958aacbcbd8a396ebd2afed9d5ef52bf
Aleksandr Popov <pishite2011@yandex.ru>
2022-09-21 21:57:27 +0500

    первый пример работы с консолью на C#

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿int number =12;
-Console.WriteLine("Месяцев в  году "  + number);
+﻿int number = 12;
+Console.WriteLine("Месяцев в  году "  + number + number);
 string n = number.ToString();
 Console.WriteLine("Введите ваше имя");
-string name = Console.ReadLine();
+string name = Console.ReadLine(); // Отдаёт всё только строкой
 Console.WriteLine("Привет, " + name);
