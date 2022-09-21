2cdc75318ceb7a796ce887d89c1f84081e1c47fb
Aleksandr Popov <pishite2011@yandex.ru>
2022-09-21 22:38:48 +0500

    добавил пример со сложением вводимых чисел

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@ string n = number.ToString();
 Console.WriteLine("Введите ваше имя");
 string name = Console.ReadLine(); // Отдаёт всё только строкой
 Console.WriteLine("Привет, " + name);
+Console.WriteLine("Сколько вам лет?");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Вам " + age + " лет.");
+
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число в");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a + b);
