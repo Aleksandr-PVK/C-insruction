445359c8bb5611f152407c61c63a2b17837b0114
Aleksandr Popov <pishite2011@yandex.ru>
2022-09-22 10:16:33 +0500

    закончил игру

--- game/Program.cs
+++ game/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Начинаем игру УГАДАЙ ЧИСЛО");
 int secret_number = new Random().Next(1,11);
-while (true)
+int attempt = 5;
+bool Win = false;
+while (attempt > 0)
 {
     Console.WriteLine("Введиете число от 1 до 10");
     int user_number = int.Parse(Console.ReadLine());
@@ -8,13 +10,27 @@ while (true)
     if(user_number > secret_number)
     {
         Console.WriteLine("Секретное число меньше");
+        attempt--;
     }
     else if(user_number < secret_number)
     {
         Console.WriteLine("Сектетное число больше");
+        attempt--;
     }
     else if(user_number == secret_number)// = присвоить == сравнить
     {
         Console.WriteLine("УГАДАЛ!!!");
+        Win = true;
+        break;
     }
+    Console.WriteLine("Попробуй ещё раз, осталось " + attempt + " попыток");
+}
+if (Win)
+{
+    Console.WriteLine("Победа!!! Поиграй ещё!");
+    Console.WriteLine("Осталось " + (attempt - 1) + " попыток");
+}else
+{
+    Console.WriteLine("Вы проиграли, секретное число было " + secret_number);
+    Console.WriteLine("Попробуй ещё раз.");
 }
